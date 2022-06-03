2b048e5fcfde934a5b86826a66fa15ad47661f40
Osemeke Echenim <echenim.osemeke@gmail.com>
2022-06-03 12:44:04 +0100

    grid and card

--- tees-place/pages/index.js
+++ tees-place/pages/index.js
@@ -1,18 +1,36 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import { Card, CardActionArea, CardMedia, Grid, CardContent, Typography, CardActions , Button } from '@mui/material'
 import Layout from './components/Layout'
+import data from '../utils/data'
 
 export default function Home() {
   return (
     <Layout>
     <div>
       <h1>Products</h1>
-      <ul>
-        <li>Product 1</li>
-        <li>Product 2</li>
-        <li>Product 3</li>
-      </ul>
+      <Grid container spacing={3}>
+        {data.products.map((product) => (
+          <Grid item md={4} key={product.name}>
+            <Card>
+              <CardActionArea>
+                <CardMedia component="img" image={product.image} title={product.name} />
+                <CardContent>
+                  <Typography>
+                    {product.name}
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+              <CardActions>
+                <Typography>
+                  ${product.price}
+                  <Button size="small" color="primary">
+                    Add to cart
+                  </Button>
+                </Typography>
+              </CardActions>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
     </div>
     </Layout>
   )
---
+++ tees-place/utils/data.js
@@ -0,0 +1,72 @@
+const data = {
+    products: [
+        {
+            name: "shirt",
+            category: "shirts",
+            price: 10,
+            brand: "nike",
+            rating: 4.5,
+            numReviews: 10,
+            countInStock: 6,
+            description: "this is a shirt",
+            image: "https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
+        },
+        {
+            name: "tight shirt",
+            category: "shirts",
+            price: 10,
+            brand: "adidas",
+            rating: 4.5,
+            numReviews: 10,
+            countInStock: 6,
+            description: "this is a shirt",
+            image: "/images/shirt1.jpg"
+        },
+        {
+            name: "black shirt",
+            category: "shirts",
+            price: 10,
+            brand: "gucci",
+            rating: 4.5,
+            numReviews: 10,
+            countInStock: 6,
+            description: "this is a shirt",
+            image: "https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
+        },
+        {
+            name: "roundneck shirt",
+            category: "pants",
+            price: 10,
+            brand: "bucket-fit",
+            rating: 4.5,
+            numReviews: 10,
+            countInStock: 6,
+            description: "this is a shirt",
+            image: "https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
+        },
+        {
+            name: "button up shirt",
+            category: "pants",
+            price: 10,
+            brand: "threadlers",
+            rating: 4.5,
+            numReviews: 10,
+            countInStock: 6,
+            description: "this is a shirt",
+            image: "https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
+        },
+        {
+            name: "easy open trouser",
+            category: "pants",
+            price: 10,
+            brand: "threadlers",
+            rating: 4.5,
+            numReviews: 10,
+            countInStock: 6,
+            description: "this is a shirt",
+            image: "https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
+        },
+        
+    ]
+}
+export default data;
